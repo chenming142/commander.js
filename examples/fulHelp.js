cfb9c8a3d9cdea6d4ab547f8fc9e6cba22c1c8c4
chenming142 <chenming142@sina.com>
2015-01-21 18:34:58 +0800

    modify file fulHelp for test.

--- examples/fulHelp.js
+++ examples/fulHelp.js
@@ -8,7 +8,7 @@ var program = require('../');
 
 program
     .alias('fullHelp')
-    .command('setup', '--setup--', 'system setup')
+    .command('setup <first> [second]',  'system setup')//TODO: 无descript时，返回值是啥？
     .usage('This for usage fullHelp')
     .version('0.0.1')
     .option('-f, --foo', 'enable some foo')
